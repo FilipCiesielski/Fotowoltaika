6041b546867ce5c308a6752bccf1e6fe297232b9
FilipCiesielski <filip.carpenter@gmail.com>
2020-01-22 21:37:58 +0100

    Updates

---
+++ precache-manifest.f7f8d5aee24334430fc91bf609a1b9a0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e511e96f2e2711c19fc2dcda820310f",
+    "url": "/Fotowoltaika/index.html"
+  },
+  {
+    "revision": "4506cfb8cdc473a6961d",
+    "url": "/Fotowoltaika/static/css/main.4d301ecb.chunk.css"
+  },
+  {
+    "revision": "c6a8e248849a2330f017",
+    "url": "/Fotowoltaika/static/js/2.73c246af.chunk.js"
+  },
+  {
+    "revision": "5599c98b7af73b31fed6a721c4f935e7",
+    "url": "/Fotowoltaika/static/js/2.73c246af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4506cfb8cdc473a6961d",
+    "url": "/Fotowoltaika/static/js/main.9a45271b.chunk.js"
+  },
+  {
+    "revision": "e485fe5a21ce82bfe66b",
+    "url": "/Fotowoltaika/static/js/runtime-main.9d1c0398.js"
+  },
+  {
+    "revision": "598e06c54366d5577aeb9af4ebdce3a6",
+    "url": "/Fotowoltaika/static/media/dlonDom-min.598e06c5.jpg"
+  },
+  {
+    "revision": "8b6411539b4b6685982ec1834a02253c",
+    "url": "/Fotowoltaika/static/media/instalacja 5 kw.8b641153.jpg"
+  },
+  {
+    "revision": "d2b03c87f0445ff19c19e67290237a17",
+    "url": "/Fotowoltaika/static/media/paneleMonter-min.d2b03c87.jpg"
+  },
+  {
+    "revision": "154960b8624a733e31edc9d2f29c2906",
+    "url": "/Fotowoltaika/static/media/paneleNiebo-min.154960b8.jpg"
+  },
+  {
+    "revision": "7a785c917baf68fae67cd8ee36ade0fb",
+    "url": "/Fotowoltaika/static/media/paneleRodzina-min.7a785c91.jpg"
+  },
+  {
+    "revision": "2015bb36fdddad1537fdfc6df60f0b42",
+    "url": "/Fotowoltaika/static/media/zyskKobieta-min.2015bb36.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fotowoltaika/precache-manifest.6768564400124dcfa199871690082fd2.js"
+  "/Fotowoltaika/precache-manifest.f7f8d5aee24334430fc91bf609a1b9a0.js"
 );
 
 self.addEventListener('message', (event) => {
